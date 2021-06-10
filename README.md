03fbd1efe8483a8d48097bfc7cd4a25c4f4caf25
Andre Vieira da Silva <andsilvadrcc@gmail.com>
2021-06-09 22:42:06 -0300

    exericio1

---
+++ README.md
@@ -0,0 +1,12 @@
+### Exercícios - Bash Script 
+
+```Bash
+$ ./relacao.sh 3 5
+3 é menor que 5
+$./relacao.sh 5 3
+5 é maior que 3
+$ ./relacao.sh 5 5
+5 é igual a 5
+$
+
+```
---
+++ exercicios/relacao.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# programa - relacao.sh:
+# Recebe dois números como parâmetro e 
+# mostra a relação entre eles. 
+
+# argument: $1
+# argument: $2
+
+echo "Running script: $0"
+
+# check if the arguments are not numbers
+if [ -n "$1" ] && [ "$1" -eq "$1" ] 2>/dev/null; then
+else
+  echo "not a number, $1"
+  exit 1
+fi
+
+if [ -n "$2" ] && [ "$2" -eq "$2" ] 2>/dev/null; then
+else
+  echo "not a number, $2"
+  exit 1
+fi
+
+
+echo "********************************"
+if [ $1 -gt $2 ]
+then
+	echo $1 é maior que $2
+fi
+
+if [ $1 -lt $2 ]
+then
+	echo $1 é menor que $2
+fi
+
+if [ $1 -eq $2 ]
+then
+	echo $1 é menor que $2
+fi
+echo "********************************"
+
+echo All done!
