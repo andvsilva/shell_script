3bb3ab934690db0d0551e7706dc5152f0e7c9d39
Andre V. Silva <andsilvadrcc@gmail.com>
2021-06-16 10:15:46 -0300

    Menu sh

---
+++ prova/menu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Menu 
+
+# 1) Mostrar total de valores.
+# 2) Mostrar elementos de um diretório.
+# 3) Mostra as máquinas que estão ativas numa rede.
+# 4) Solicita o nome de um grupo e mostra o numero de cadastros com esse nome.
+# 5) Sair do Sistema.
+
+# 6) Opção Invalida e voltar a exibir o menu.
+
+# String with the Menu
+Menu="1) Mostrar total de valores.\n
+2) Mostrar elementos de um diretório.\n
+3) Mostra as máquinas que estão ativas numa rede.\n
+4) Solicita o nome de um grupo e mostra o numero de cadastros com esse nome.\n
+5) Sair do Sistema.\n"
+
+
+
